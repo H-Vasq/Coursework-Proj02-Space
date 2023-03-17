9f41e7cab9a25ca6b4359d66d1e9934b6f926dd9
H-Vasq <123214691+H-Vasq@users.noreply.github.com>
2023-03-17 11:40:44 -0600

    Button behaviours add

---
+++ main.js
@@ -0,0 +1,4 @@
+const btn = document.getElementById("myButton"); // select the button by its id
+btn.addEventListener("click", function() { // add an event listener for click event
+  alert("THIS IS JUST A TEST"); // show an alert message when clicked
+});
